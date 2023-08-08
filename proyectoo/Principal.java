506e628decb040aee5588f0887e8121ae5cf2ca8
Leogabii <irmalicia.sanchez2021@gmail.com>
2023-08-08 15:56:08 -0300

    version_1

---
+++ proyectoo/Principal.java
@@ -0,0 +1,17 @@
+public class Principal {
+
+    private String nombre;
+
+    public Principal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
